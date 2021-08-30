9684f2f109d1e10130fd7706fa0c39f83749daf8
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2021-08-30 08:33:08 +0800

    room concept

--- server/index.js
+++ server/index.js
@@ -1,26 +1,64 @@
-const httpServer = require('http').createServer()
-const io = require('socket.io')(httpServer, {
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000'
-  }
-})
-
-io.on('connection', socket => {
-  console.log(socket.id + ' just connected')
-
-  socket.on('disconnect', () => {
-    console.log('a user disconnected')
-  })
-
-})
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+    origin: "http://localhost:8080",
+  },
 });
 
-httpServer.listen(3000, () => {
-  console.log('Listening to port 3000')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) return next(new Error("invalid username"));
+
+  socket.username = username;
+  next();
+});
+
+io.on("connection", (socket) => {
+  const rooms = [];
+
+  for (let [id, socket] of io.of("/").sockets) {
+    rooms.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+
+  // socket.emit("users", rooms);
+
+  // socket.on("disconnecting", (reason) => {
+  //   for (const room of rooms) {
+  //     if (room !== socket.id) socket.to(room).emit("user has left", socket.id);
+  //   }
+  // });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("private message", ({ username, message, recipient }) => {
+    recipient = rooms.find((room) => {
+      console.log(room, 2);
+      return room.username === recipient;
+    });
+
+    io.to(recipient.userID).emit("private message", {
+      from: username,
+      content: message,
+    });
+  });
+
+  io.emit("rooms", rooms);
+  console.log(rooms);
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
--- src/App.js
+++ src/App.js
@@ -1,20 +1,87 @@
-import { useEffect } from 'react';
-import socket from './socket';
+import socket from "./socket";
+import { useState, useEffect, useRef } from "react";
 
 export default function App() {
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [recipient, setRecipient] = useState("");
+  // const [messages, setMessages] = useState([]);
+  const [showChat, setShowChat] = useState(false);
+
+  // const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({ username: "", messages: [] });
 
   useEffect(() => {
-    socket.connect();
+    socket.on("private message", ({ from, content }) => {
+      user.messages.push({ from, content });
+      setUser({ ...user });
+    });
+  }, [user.messages.length]);
 
-    return () => {
-      socket.close()
-    }
-  });
-  
-  if (socket.connected)
-    console.log('hey')
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    socket.username = username;
+    socket.emit("private message", { username, message, recipient });
+    setMessage("");
+  };
+
+  const connectToServer = (e) => {
+    e.preventDefault();
+    socket.auth = { username };
+    socket.connect();
+    setShowChat(true);
+  };
 
   return (
-    <div>Hello World</div>
-  )
+    <div>
+      <h1>Chat</h1>
+      {showChat ? (
+        <form onSubmit={sendMessage}>
+          <br />
+          <label>
+            {" "}
+            Recipient
+            <input
+              type="text"
+              onChange={(e) => setRecipient(e.target.value)}
+              value={recipient}
+            />
+          </label>
+          <br />
+          <label>
+            Message
+            <input
+              type="text"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            />
+          </label>
+          <button>Submit</button>
+        </form>
+      ) : (
+        <form onSubmit={connectToServer}>
+          <label>
+            {" "}
+            Username
+            <input
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
+            <button>Submit</button>
+          </label>
+        </form>
+      )}
+
+      <div>
+        {user.messages.map((message, index) => (
+          <pre key={index}>
+            {message.from}
+            {message.content}
+          </pre>
+        ))}
+      </div>
+    </div>
+  );
 }
